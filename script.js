110a9672eabfbd23bec3b67bff9b0fc8d3fc618d
alex <avon1462@gmail.com>
2025-02-12 22:30:00 -0800

    chore: add log

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Javascript to trigger shake effect
 document.querySelector(".no-btn").addEventListener("click", function(){
+  console.log("button clicked");
     this.classList.add('shake');
   setTimeout(() => {
     this.classList.remove('shake'); // Remove shake class after animation
   }, 500); // Remove after 500ms, or the duration of the animation
-})
+});
