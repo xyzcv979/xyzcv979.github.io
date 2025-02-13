59140aaad40d92110ec2db92e935d3635983a4fd
alex <avon1462@gmail.com>
2025-02-12 22:15:19 -0800

    feature: add button shake

---
+++ script.js
@@ -0,0 +1,7 @@
+// Javascript to trigger shake effect
+document.querySelector(".no-btn").addEventListener("click", function(){
+    this.classList.add('shake');
+  setTimeout(() => {
+    this.classList.remove('shake'); // Remove shake class after animation
+  }, 500); // Remove after 500ms, or the duration of the animation
+})
